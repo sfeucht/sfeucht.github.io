eb2bb3dfd5f3fca35bcfc9fb482a444798e6f75b
Sheridan Feucht <sfeucht@Sheridans-MacBook-Air.local>
2024-12-14 19:40:02 -0500

    books

---
+++ content/books2024.md
@@ -0,0 +1,46 @@
+---
+disableComments: true
+---
+
+# Books I Read in 2024
+
+Here are the books that I remember reading this year. I don't set "reading goals" for myself, but this year a lot of interesting books fell into my lap. I even managed to have time to read many of them! 
+
+- East, West - Salman Rushdie
+- If This Is a Man - Primo Levi
+- How to Do Things with Words - JL Austin
+- Lolita - Vladimir Nabokov
+- Seeing Further - Esther Kinsky
+- The Empusium - Olga Tokarczuk 
+- Childish Literature - Alejandro Zambra 
+- The Ways of Paradise - Peter Cornell 
+- Attached - Amir Levine and Rachel S. F. Heller 
+
+
+<!-- ### East, West - Salman Rushdie.
+
+My partner Adrian bought this from a bin in Harvard Square. I found it delightful, and will be looking for more Salman Rushdie in the future. 
+
+**If This Is a Man - Primo Levi.** Lent to me by my friend Raj. 
+
+**How to do things with words - JL Austin.** Lent to me by my advisor David. Makes me want to read Judith Butler. 
+
+**Lolita - Nabokov.** One of the best novels I've read so far, ever. 
+
+**Seeing Further - Esther Kinsky.** Gift from my partner (first book from a Fitzcarraldo Editions subscription). 
+
+**The Empusium - Olga Tokarczuk.** 
+
+**Childish Literature - Alejandro Zambra.**
+
+**The Ways of Paradise - Peter Cornell.**
+
+**Attached - Amir Levine and Rachel S. F. Heller.**  -->
+
+## Unfinished  
+Here are some books I enjoyed this year but didn't finish. I'm hoping to pick at least a few of them back up next year. 
+
+- The Art of Dramatic Writing - Lajos Egri
+- 3 Shades of Blue: Miles Davis, John Coltrane, Bill Evans, and the Lost Empire of Cool - James Kaplan
+- The Enigma of Reason - Dan Sperber and Hugo Mercier
+- Psychology and the East - Carl Jung 
---
+++ hugo.rb
@@ -0,0 +1,43 @@
+class Hugo < Formula
+  desc "Configurable static site generator"
+  homepage "https://gohugo.io/"
+  url "https://github.com/gohugoio/hugo.git",
+      tag:      "v0.121.0",
+      revision: "e321c3502aa8e80a7a7c951359339a985f082757"
+  license "Apache-2.0"
+  head "https://github.com/gohugoio/hugo.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5dd5199ebfee2de5a900f81c01f0a944849d1627eb1cdfad7c4f57522243082f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5011a8049b5daff4c1e5192376c384bcca6bc2f5ecaedb35a50a4b5da6ac119e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d603c2c1aae64bbabaaf092377ae2e8955bfe9ed5cf3cb58bd80e63b5f9bbf2a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3a2628d8d83ea39f7313e5ba63d2eca11ea34af6c79e9189ad22bd2d2a2ad5b9"
+    sha256 cellar: :any_skip_relocation, ventura:        "c93dc4be70e0c76f12cc48a0006df403b2d39ec93db3e6f70b56d25e9ad4e8d1"
+    sha256 cellar: :any_skip_relocation, monterey:       "72e7f38ed5f12ad6f17d395e3ebf5f3afa0ac12ba292d41406fc1755b504a9ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b53f30bddfb35fbdec42c75a8de755d171f2353f2c8b47eb216c07f5f294b389"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/gohugoio/hugo/common/hugo.commitHash=#{Utils.git_head}
+      -X github.com/gohugoio/hugo/common/hugo.buildDate=#{time.iso8601}
+      -X github.com/gohugoio/hugo/common/hugo.vendorInfo=brew
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "-tags", "extended"
+
+    generate_completions_from_executable(bin/"hugo", "completion")
+    system bin/"hugo", "gen", "man", "--dir", man1
+  end
+
+  test do
+    site = testpath/"hops-yeast-malt-water"
+    system bin/"hugo", "new", "site", site
+    assert_predicate site/"hugo.toml", :exist?
+
+    assert_match version.to_s, shell_output(bin/"hugo version")
+  end
+end
+
